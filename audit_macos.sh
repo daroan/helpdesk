898f119956c10146ef79512fbf51f41f9747bd31
daroan <78073060+daroan@users.noreply.github.com>
2024-03-05 12:40:27 +0000

    MacOS Audit Hardware Information

---
+++ audit_macos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Apaga ultimo arquivo
+rm ~/Desktop/audit.txt
+
+# ObtŽm informa›es do sistema
+timenow=$(date '+%Y-%m-%d %X UTC')
+
+model=$(sysctl -n hw.model)
+gpu=$(system_profiler SPDisplaysDataType | grep Chipset)
+cpu=$(sysctl -n machdep.cpu.brand_string)
+resolution=$(system_profiler SPDisplaysDataType | awk '/Resolution:/ {print $2, $3, $4}')
+ram=$(sysctl -n hw.memsize | awk '{print $0/1024^3 " GB"}')
+disk=$(system_profiler SPNVMeDataType)
+battery_capacity=$(system_profiler SPPowerDataType | grep -A3 -B7 "Condition")
+
+# ObtŽm o nœmero de sŽrie do Mac
+serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+
+# Escreve as informa›es no arquivo de texto
+echo "Datetime: $timenow" >> ~/Desktop/audit.txt
+echo "Serial_Number: $serial_number" >> ~/Desktop/audit.txt
+echo "Model: $model" >> ~/Desktop/audit.txt
+echo "Graphic_Card: $gpu" >> ~/Desktop/audit.txt
+echo "CPU: $cpu" >> ~/Desktop/audit.txt
+echo "Screen_Resolution: $resolution" >> ~/Desktop/audit.txt
+echo "RAM: $ram" >> ~/Desktop/audit.txt
+echo "Storage: $disk" >> ~/Desktop/audit.txt
+echo "Battery: $battery_capacity" >> ~/Desktop/audit.txt
+
+
