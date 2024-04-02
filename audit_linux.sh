ee192af77ab2c8ec53d4f7651a7819ea28b6085d
daroan <78073060+daroan@users.noreply.github.com>
2024-04-02 12:09:43 +0100

    Update audit_linux.sh

--- audit_linux.sh
+++ audit_linux.sh
@@ -1,6 +1,13 @@
 
 #!/bin/bash
 
-sudo inxi -Fxxc 0 > audit_test.txt
+# From site https://lindevs.com/install-inxi-on-ubuntu/
+
+sudo wget -O /usr/local/bin/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
+
+sudo chmod a+x /usr/local/bin/inxi
+
+# Our command to get hardware specifications
+sudo inxi -Fxxc 0 > Desktop/audit_test.txt
 
 
