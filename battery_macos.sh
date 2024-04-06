93022de8d2b0ff897bfd69f012370e2ab22eb589
daroan <78073060+daroan@users.noreply.github.com>
2024-04-06 14:30:21 +0100

    Update battery_macos.sh

--- battery_macos.sh
+++ battery_macos.sh
@@ -20,7 +20,7 @@ while true; do
     echo "$battery_info" >> ~/Desktop/battery_${serial_number}.txt
 
     # Enviar para Nextcloud
-    curl -u ${username}:${password} -T ~/Desktop/battery_${serial_number}.txt https://cloud01.opsdata.ch/remote.php/dav/files/atlasprodigio/
+    curl -u ${username}:${password} -T ~/Desktop/battery_${serial_number}.txt https://cloud01.opsdata.ch/remote.php/dav/files/$username/
     
     # Espera 5 minutos
     echo $timenow
