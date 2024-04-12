52b51b8f020c25deeaa7328499e640a7fda91dc2
daroan <78073060+daroan@users.noreply.github.com>
2024-04-12 11:34:05 +0100

    Update stress_linux.sh

--- stress_linux.sh
+++ stress_linux.sh
@@ -18,8 +18,8 @@ flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flath
 
 flatpak install -y flathub com.leinardi.gst
 
-sudo gnome-screenshot --delay=3700 --file=$log_file
-
 firefox https://cloud01.opsdata.ch/index.php/login
+
+sudo gnome-screenshot -i
 
 flatpak run com.leinardi.gst
