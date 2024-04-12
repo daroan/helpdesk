cbc3a058c770a3ab0927c3f7b13dc005c914da8d
daroan <78073060+daroan@users.noreply.github.com>
2024-04-12 11:13:35 +0100

    Update stress_linux.sh

--- stress_linux.sh
+++ stress_linux.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Obter o número de série do PC
+pc_serial=$(sudo dmidecode -s system-serial-number)
+
+# Nome do arquivo de log com o número de série do PC
+log_file=Desktop/"xstress_${pc_serial}.png"
+
+sudo rm $log_file
+
 sudo apt update
 
 sudo apt install -y flatpak
+
+sudo apt install -y gnome-screenshot
 
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub com.leinardi.gst
 
 flatpak run com.leinardi.gst
+
+sleep 3600
+
+gnome-screenshot -f ~/Desktop/xstress.png
