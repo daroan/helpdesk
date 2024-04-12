db38c43385110921598a0ba4989df838295e04b9
daroan <78073060+daroan@users.noreply.github.com>
2024-04-12 10:32:41 +0100

    Update audit_macos.sh

--- audit_macos.sh
+++ audit_macos.sh
@@ -14,15 +14,19 @@ battery_capacity=$(system_profiler SPPowerDataType | grep -A3 -B7 "Condition")
 # ObtŽm o nœmero de sŽrie do Mac
 serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 
+# remove oldest file
+rm ~/Desktop/${serial_number}_audit_mac.txt
+
 # Escreve as informa›es no arquivo de texto
-echo "Datetime: $timenow" >> ~/Desktop/audit_mac.txt
-echo "Serial_Number: $serial_number" >> ~/Desktop/audit_mac.txt
-echo "Model: $model" >> ~/Desktop/audit_mac.txt
-echo "Graphic_Card: $gpu" >> ~/Desktop/audit_mac.txt
-echo "CPU: $cpu" >> ~/Desktop/audit_mac.txt
-echo "Screen_Resolution: $resolution" >> ~/Desktop/audit_mac.txt
-echo "RAM: $ram" >> ~/Desktop/audit_mac.txt
-echo "Storage: $disk" >> ~/Desktop/audit_mac.txt
-echo "Battery: $battery_capacity" >> ~/Desktop/audit_mac.txt
+echo "Datetime: $timenow" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Serial_Number: $serial_number" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Model: $model" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Graphic_Card: $gpu" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "CPU: $cpu" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Screen_Resolution: $resolution" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "RAM: $ram" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Storage: $disk" >> ~/Desktop/${serial_number}_audit_mac.txt
+echo "Battery: $battery_capacity" >> ~/Desktop/${serial_number}_audit_mac.txt
 
 
+>> ~/Desktop/${serial_number}_audit_mac.txt
