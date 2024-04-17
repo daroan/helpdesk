7855062fefe0096ee3150bdc2e7b63f782861a97
daroan <78073060+daroan@users.noreply.github.com>
2024-04-17 09:12:49 +0100

    Update stress_macos.sh

--- stress_macos.sh
+++ stress_macos.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-open -a Endurance
+curl https://raw.githubusercontent.com/daroan/helpdesk/master/Endurance.app.zip -o ~/Endurance.app.zip
+
+unzip ~/Endurance.app.zip
+
+rm -rf ~/Endurance.app.zip
+
+open -a ~/Endurance
+
+rm -rf ~/Endurance.app
