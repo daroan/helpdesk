ab7f83fa9dee3ee1517a0d95264ea82fca458748
daroan <78073060+daroan@users.noreply.github.com>
2024-04-17 08:58:33 +0100

    Update audit_macos.sh

--- audit_macos.sh
+++ audit_macos.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-# ObtŽm informa›es do sistema
+# Collect Information in Variables
 timenow=$(date '+%Y-%m-%d %X UTC')
-
-model=$(sysctl -n hw.model)
-gpu=$(system_profiler SPDisplaysDataType | grep Chipset)
-cpu=$(sysctl -n machdep.cpu.brand_string)
-resolution=$(system_profiler SPDisplaysDataType | awk '/Resolution:/ {print $2, $3, $4}')
-ram=$(sysctl -n hw.memsize | awk '{print $0/1024^3 " GB"}')
-disk=$(system_profiler SPNVMeDataType)
+gpu=$(system_profiler SPDisplaysDataType)
+disk=$(system_profiler SPNVMeDataType | grep -B200 "Detachable Drive")
 battery_capacity=$(system_profiler SPPowerDataType | grep -A3 -B7 "Condition")
-hardwaredatatype=$(system_profiler SPHardwareDataType)
-
-# ObtŽm o nœmero de sŽrie do Mac
+hardware_data=$(system_profiler SPHardwareDataType)
 serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 
 # remove oldest file
 rm ~/Desktop/${serial_number}_audit_mac.txt
 
-# Escreve as informa›es no arquivo de texto
-echo "Datetime: $timenow" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Serial_Number: $serial_number" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Model: $model" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Graphic_Card: $gpu" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "CPU: $cpu" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Screen_Resolution: $resolution" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "RAM: $ram" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Storage: $disk" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Battery: 
-$battery_capacity" >> ~/Desktop/${serial_number}_audit_mac.txt
-echo "Hardware Data Type:
-$hardwaredatatype" >> ~/Desktop/${serial_number}_audit_mac.txt
+# Write information in text file inside Desktop
+echo "Datetime: $timenow" >> ~/Desktop/audit_mac.txt
 
+echo "
+$hardware_data" >> ~/Desktop/audit_mac.txt
+
+echo "
+$gpu" >> ~/Desktop/audit_mac.txt
+
+echo "
+Storage: $disk" >> ~/Desktop/audit_mac.txt
+
+echo "
+Battery: 
+$battery_capacity" >> ~/Desktop/audit_mac.txt
+
+grep . ~/Desktop/audit_mac.txt >> ~/Desktop/${serial_number}_audit_mac.txt
+
+rm ~/Desktop/audit_mac.txt
