0365d9fb9c1280f7f502dc463e695520d92c4591
daroan <78073060+daroan@users.noreply.github.com>
2024-04-17 10:03:18 +0100

    Update audit_macos.sh

--- audit_macos.sh
+++ audit_macos.sh
@@ -7,12 +7,14 @@ disk=$(system_profiler SPNVMeDataType | grep -B200 "Detachable Drive")
 battery_capacity=$(system_profiler SPPowerDataType | grep -A3 -B7 "Condition")
 hardware_data=$(system_profiler SPHardwareDataType)
 serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+cpu=$(sysctl -n machdep.cpu.brand_string)
 
 # remove oldest file
 rm ~/Desktop/${serial_number}_audit_mac.txt
 
 # Write information in text file inside Desktop
 echo "Datetime: $timenow" >> ~/Desktop/audit_mac.txt
+echo "CPU: $cpu" >> ~/Desktop/audit_mac.txt
 
 echo "
 $hardware_data" >> ~/Desktop/audit_mac.txt
